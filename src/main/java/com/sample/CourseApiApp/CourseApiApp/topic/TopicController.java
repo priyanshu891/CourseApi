497f45bd56ac64775916e2695d054dab17ba8ff5
Priyanshu Rai <Priyanshu.rai891@gmail.com>
2018-06-04 21:26:22 +0530

    Implemented Update and Delete Method

--- src/main/java/com/sample/CourseApiApp/CourseApiApp/topic/TopicController.java
+++ src/main/java/com/sample/CourseApiApp/CourseApiApp/topic/TopicController.java
@@ -1,6 +1,5 @@
 package com.sample.CourseApiApp.CourseApiApp.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +27,16 @@ public class TopicController {
 	@RequestMapping(method=RequestMethod.POST,value="/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
-		
 	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+	
 }
--- src/main/java/com/sample/CourseApiApp/CourseApiApp/topic/TopicService.java
+++ src/main/java/com/sample/CourseApiApp/CourseApiApp/topic/TopicService.java
@@ -15,20 +15,41 @@ private	List<Topic> topics = new ArrayList<>( Arrays.asList(
 			new Topic("Javascript","Node JS","AngularJS, NodeJS,MEAN Stack"),
 			new Topic("HTML","HTML & CSS","HTML 5 & CSS 3")
 			));
+/*-----------------Get All Topics--------------------*/
 
 public List<Topic> getAllTopics() {
 	return topics;
 }
+
+/*-----------------Get a Topic----------------------*/
 
 public Topic getTopic(String id)
 {
 return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();	
 }
 
+/*-----------------Adding Topic--------------------*/
+
 public void addTopic(Topic topic) {
-	// TODO Auto-generated method stub
-	topics.add(topic);
-	
+	topics.add(topic);	
+}
+
+/*-----------------Updating Topic--------------------*/
+
+public void updateTopic(String id, Topic topic) {
+	for(int i=0;i < topics.size();i++) {
+		Topic t = topics.get(i);
+		if(t.getId().equals(id)) {
+			topics.set(i, topic);
+			return;
+		}
+	}
+}
+
+/*-----------------Deleting Topic--------------------*/
+
+public void deleteTopic(String id) {
+topics.removeIf(t -> t.getId().equals(id));
 }
 
 }
